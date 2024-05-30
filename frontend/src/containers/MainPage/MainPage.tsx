ed58f76d3a12fec9874a696a8f8295724d6f46e8
lilys012 <lysianthus@naver.com>
2024-05-30 23:25:42 +0900

    0530

--- frontend/src/containers/MainPage/MainPage.tsx
+++ frontend/src/containers/MainPage/MainPage.tsx
@@ -5,12 +5,51 @@ import { AppDispatch } from "../../store";
 
 import "./MainPage.scss";
 import { Layout, Checkbox, Space, Button } from "antd";
-import { ArrowLeftOutlined, SyncOutlined } from "@ant-design/icons";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SearchBar from "material-ui-search-bar";
+import { styled } from "@material-ui/core/styles";
+
+const { Header, Content } = Layout;
+
+export const CustomSearchBar = styled(SearchBar)({
+    backgroundColor: "#F5F5F5",
+    borderRadius: "10px",
+    fontFamily: '"NanumGothic", sans-serif',
+    fontSize: "10px",
+});
 
 function MainPage() {
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    const onClickClose = () => {
+        setSearchQuery("");
+    };
+    const onSubmitSearchBox = () => {};
+
     return (
         <Layout className="MainPage">
-            <p>MainPage</p>
+            <Header
+                id="mainpage-upper-container"
+                className="Header"
+                style={{ backgroundColor: "white" }}
+            >
+                <div id="logo-container">
+                    <img src="/RXLogo.svg" />
+                </div>
+                <div id="search-box-container">
+                    <CustomSearchBar
+                        className="search-box"
+                        value={searchQuery}
+                        onChange={(searchVal) => setSearchQuery(searchVal)}
+                        onCancelSearch={() => onClickClose()}
+                        onRequestSearch={() => onSubmitSearchBox()}
+                        placeholder=""
+                    />
+                </div>
+                <div></div>
+            </Header>
         </Layout>
     );
 }
