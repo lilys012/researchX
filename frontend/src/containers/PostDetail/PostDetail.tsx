c010a6ca19447cae04b9dffc51df798c6fdcc986
lilys012 <lysianthus@naver.com>
2024-06-04 00:35:49 +0900

    conflict resolve

--- frontend/src/containers/PostDetail/PostDetail.tsx
+++ frontend/src/containers/PostDetail/PostDetail.tsx
@@ -4,10 +4,11 @@ import PostOverview, {
 } from "../../components/PostOverview/PostOverview";
 import HeaderComponent from "../../components/Header/Header";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { PostType, selectPost } from "../../store/slices/post";
 import GPTInteraction from "../../components/GPTInteraction/GPTInteraction";
 import "./PostDetail.scss";
+import { AppDispatch } from "../../store";
 
 function PostDetail() {
     const { id } = useParams();
@@ -28,27 +29,29 @@ function PostDetail() {
         created_at: "",
         users: [],
     });
-  useEffect(() => {
+    useEffect(() => {
         postState.posts.forEach((e) => {
             if (id && e.id === parseInt(id)) setTargetPost(e);
         });
     }, [id]);
-    return <div id='PostDetail'>
-        <HeaderComponent setCurKeyword={null} setRefresh={null} />
-        <div className="postdetail-main-container">
-            <div className="upper">
-                <div className="postdetail-overview">
-                    <PostOverview post={targetPost} overview={false} />
+    return (
+        <div id="PostDetail">
+            <HeaderComponent setCurKeyword={null} setRefresh={null} />
+            <div className="postdetail-main-container">
+                <div className="upper">
+                    <div className="postdetail-overview">
+                        <PostOverview post={targetPost} overview={false} />
+                    </div>
+                    <div className="gpt-container">
+                        <GPTInteraction />
+                    </div>
                 </div>
-                <div className="gpt-container">
-                    <GPTInteraction />
+                <div className="lower">
+                    <div className="author-bio">Author Bio</div>
                 </div>
-            </div>
-            <div className="lower">
-                <div className="author-bio">Author Bio</div>
             </div>
         </div>
-    </div>;
+    );
 }
 
 export default PostDetail;
